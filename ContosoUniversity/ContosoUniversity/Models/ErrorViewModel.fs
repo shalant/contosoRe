af5139200ffbf9f87f11af2d9167614b6cbd325e
shalant <doug.rosenberg@gmail.com>
2022-10-13 14:10:21 -0500

    initial

---
+++ ContosoUniversity/ContosoUniversity/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ContosoUniversity.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
